3a515d64531153d16e874d4f9bc79030ca075da3
syee <stevenyee64@gmail.com>
2020-04-26 14:49:26 -0700

    Added graph for 3d

--- assignment3/main.m
+++ assignment3/main.m
@@ -1,3 +1,10 @@
+%%
+%Steven Yee
+%Macro C PS 3
+%4/26/20
+
+%%
+%Problem 2
 clearvars
 
 % Option 1: Write different files for different parameters
@@ -55,3 +62,67 @@ plot(irfs_rho4.p_eps)
 title('IRF Price')
 legend('nu=0.25', 'nu=0.5', 'nu=1', 'nu=2', 'nu=4' ,'Location', 'SouthOutside','Orientation','horizontal','Box','off')
 hold off
+saveas(gcf,'macroPS3graphs.png')
+
+%%
+%Problem 3
+
+gm = linspace(0.001,20,1000)
+a = 0;
+b = 1/3;
+r = 1;
+i = r + gm;
+lnY = 1;
+ln_mp = a - b*i + lnY;
+S = exp(ln_mp + log(gm));
+figure;
+hold on
+plot(gm, S)
+% title('Real Seniorage vs. gm: r = 1')
+
+gm = linspace(0.001,20,1000)
+a = 0;
+b = 1/3;
+r = 2;
+i = r + gm;
+lnY = 1;
+ln_mp = a - b*i + lnY;
+S = exp(ln_mp + log(gm));
+plot(gm, S)
+% title('Real Seniorage vs. gm: r = 2')
+
+gm = linspace(0.001,20,1000)
+a = 0;
+b = 1/3;
+r = 3;
+i = r + gm;
+lnY = 1;
+ln_mp = a - b*i + lnY;
+S = exp(ln_mp + log(gm));
+plot(gm, S)
+% title('Real Seniorage vs. gm: r = 3')
+
+gm = linspace(0.001,20,1000)
+a = 0;
+b = 1/3;
+r = 4;
+i = r + gm;
+lnY = 1;
+ln_mp = a - b*i + lnY;
+S = exp(ln_mp + log(gm));
+plot(gm, S)
+title('Real Seniorage vs. gm')
+legend('r = 1', 'r =2', 'r = 3', 'r = 4', 'Location', 'SouthOutside','Orientation','horizontal','Box','off')
+hold off
+saveas(gcf,'macroCPS3seniorage.png')
+
+
+
+
+
+
+
+
+
+
+
